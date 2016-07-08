67905bea1a7558b2baadd02e7022fd8872c55a3b
Shiddiq <shiddiqitt@gmail.com>
2016-07-08 16:28:49 +0700

    Add files via upload

---
+++ src/main/webapp/WEB-INF/resources/js/application.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+ var mapOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(-34.397, 150.644),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+})
